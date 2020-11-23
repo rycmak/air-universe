a45c8a7106a9df355776df7c8af3cfb124f4fc35
Rachel Mak <ryc.mak@gmail.com>
2020-11-23 19:26:20 +1100

    Added review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_065748) do
+ActiveRecord::Schema.define(version: 2020_11_23_081027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2020_11_23_065748) do
     t.index ["owner_id"], name: "index_galaxies_on_owner_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.integer "rating"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,4 +66,5 @@ ActiveRecord::Schema.define(version: 2020_11_23_065748) do
   add_foreign_key "bookings", "galaxies"
   add_foreign_key "bookings", "users", column: "customer_id"
   add_foreign_key "galaxies", "users", column: "owner_id"
+  add_foreign_key "reviews", "bookings"
 end
