eaa99edb28e30d323d05a4f79b873b0e33c5de94
amani anai <amani@dfi.agency>
2020-11-24 21:33:16 -0600

    FEATURE: routes.rb updated with :destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :galaxies, only: [ :index, :show ] do
+  resources :galaxies, only: [ :index, :show,  :destroy ] do
     resources :bookings, only: [ :new, :create ]
   end
   devise_for :users
   resources :users, only: [ :show ] do
-    resources :galaxies, only: [ :index, :new, :create, :destory ]
+    resources :galaxies, only: [ :index, :new, :create,]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
